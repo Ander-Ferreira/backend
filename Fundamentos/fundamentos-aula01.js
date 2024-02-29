8919e6d2a9ec5dcaf01e105c23044de5c4465ce3
unknown <23114290208@ACADEMICO.IESB.BR>
2024-02-29 20:24:21 -0300

    criação da aula 02

--- Fundamentos/fundamentos-aula01.js
+++ Fundamentos/fundamentos-aula01.js
@@ -1,6 +1,8 @@
-console.log("TESTE01")
+console.log("TESTE01") ; console.log('teste 002')
 console.log("TESTE02")
 console.log("TESTE03")
 console.log("TESTE04")
 console.log("TESTE05")
+
+
 
---
+++ Fundamentos/variaveis.js
@@ -0,0 +1,21 @@
+//Var
+
+//code runner, extension settings, run in terminal opção
+
+var notaA1 = 10
+
+const nome = 'Lucas Garcia'
+const nota = 6
+let aprovado = false
+
+if(nota >= 6){
+    aprovado = true
+    
+
+}
+
+console.log("Nota: " + nota)
+console.log('Nome: ' + nome)
+console.log('Aprovado? ' + aprovado)
+
+
