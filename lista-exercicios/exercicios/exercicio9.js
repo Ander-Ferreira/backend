04de6fc37b1d1061e95b8d5ab1d07dba99931440
Anderson Ferreira <andersonadm1314@gmail.com>
2024-03-09 12:23:58 -0300

    Adicionando resolução do exercício 9

---
+++ lista-exercicios/exercicios/exercicio9.js
@@ -0,0 +1,16 @@
+/*
+9. Faça um script para somar dois números e multiplicar o resultado pelo primeiro número.
+*/
+
+let numeroA = 2
+
+let numeroB = 2
+
+const soma = function(multi, adicao){
+    adicao = (numeroA + numeroB) * numeroA
+    console.log(`O resultado é ${adicao}`)
+    return
+
+}
+
+soma()
