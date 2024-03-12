ea4bc5978941cc68291946f85b940ba2c1d2207a
Ander-Ferreira <andersonadm1314@gmail.com>
2024-03-11 21:51:48 -0300

    Aula 04

---
+++ api-exemplos/api.js
@@ -0,0 +1,22 @@
+
+//Criando um servidor na minha própria máquina
+//nodejs.org/docs
+
+const server = require('http')
+
+let contador = 0
+
+server.createServer((req, res) => {
+
+contador++
+
+console.log('Contador de requisições: ', contador)
+
+res.write('Bem-vindo ao meu backend. Contador de requisições: ' + contador)
+res.end()
+
+
+}
+
+
+).listen(3000)
