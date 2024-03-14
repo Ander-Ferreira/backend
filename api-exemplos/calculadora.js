b24602757ea2922f32dd69027b00e3659ed5f94b
Ander-Ferreira <andersonadm1314@gmail.com>
2024-03-14 19:57:37 -0300

    esquema de módulos do node.js

---
+++ api-exemplos/calculadora.js
@@ -0,0 +1,34 @@
+const somar = function (numeroA, numeroB){
+    return numeroA + numeroB
+
+
+
+}
+
+const subtrair = function (numeroA, numeroB){
+    return numeroA - numeroB
+
+
+
+}
+
+
+const multiplicar = function (numeroA, numeroB){
+    return numeroA * numeroB
+
+
+
+}
+
+const dividir = function (numeroA, numeroB){
+    return numeroA / numeroB
+
+
+
+}
+
+//Exportando as funções que criei como módulo
+module.exports = {
+     somar, subtrair, multiplicar, dividir
+
+}
---
+++ api-exemplos/usarCalculadora.js
@@ -0,0 +1,8 @@
+const calculadora = require('./calculadora') //importando o módulo que criamos em calculadora.js
+
+//usando os módulos
+console.log(calculadora.somar(2,2))
+console.log(calculadora.subtrair(2,2))
+console.log(calculadora.multiplicar(2,2))
+console.log(calculadora.dividir(2,2))
+
