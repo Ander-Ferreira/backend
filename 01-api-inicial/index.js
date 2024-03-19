3b61f4854db054faf09f876eabfe65a36c0f6320
Anderson Ferreira <andersonadm1314@gmail.com>
2024-03-19 15:56:45 -0300

    adicionando até o exercício 05 de query

--- 01-api-inicial/index.js
+++ 01-api-inicial/index.js
@@ -111,12 +111,6 @@ requisicao.send(media)
 */
 
 
-app.listen(3000, () => {
-    console.log('Api iniciada! Rodando em http://localhost:3000') 
-
-
-})
-
 //Exercicio 01 modo query.params
 
 //Chamando no postman http://localhost:3000/exercicio1?nota1=5&nota2=3&nota4=3
@@ -125,10 +119,10 @@ app.get('/exercicio1', (requisicao, resposta) =>{
 
     console.log(requisicao.query)
 
-    const nota1 = Number(requisicao.query)
-    const nota2 = Number(requisicao.query)
-    const nota3 = Number(requisicao.query)
-    const nota4 = Number(requisicao.query)
+    const nota1 = Number(requisicao.query.nota1)
+    const nota2 = Number(requisicao.query.nota2)
+    const nota3 = Number(requisicao.query.nota3)
+    const nota4 = Number(requisicao.query.nota4)
 
     let media = (nota1 + nota2 + nota3 + nota4) / 4
 
@@ -136,4 +130,106 @@ app.get('/exercicio1', (requisicao, resposta) =>{
 
     resposta.send(`media: ${media},
     mensagem:${mensagem}`)
+})
+
+/*2. Escreva um script para ler o número total de eleitores de um município, 
+o número de votos brancos, nulos e válidos. Calcular e escrever o percentual 
+que cada um representa em relação ao total de eleitores. */
+
+app.get('/eleicao', (requisicao, resposta) => {
+    console.log(resposta.query) //exibe minha resposta com a query no terminal, só serve para isso, testei.
+    
+    const votosBrancos = Number(requisicao.query.votosBrancos) //transformo a query em Number para não dar NaN
+    const votosNulos = Number(requisicao.query.votosNulos)
+    const votosValidos = Number(requisicao.query.votosValidos)
+
+    const total = votosBrancos + votosNulos + votosValidos
+        
+    
+    //Calculando o percentual
+    const percentual = (votosValidos * total) / 100
+    const percentual2 = (votosNulos * total) / 100
+    const percentual3 = (votosBrancos * total) / 100
+    
+    //Preciso jogar todo o resultado em uma resposta.send só, se eu colocar mais de uma resposta.send, só vai pegar uma
+    resposta.send(`Os votos totais são ${total}%,<br>
+            Os votos Válidos são ${percentual}%,<br>
+            Os votos Nulos são ${percentual2}%,<br>
+            Os votos Brancos são ${percentual3}%`)
+    
+})
+
+
+/*3. Escreva um script para ler o salário mensal atual de um funcionário e o percentual de reajuste. 
+Calcular e escrever o valor do novo salário.
+*/
+
+app.get('/exercicio3', (requisicao, resposta) => {
+    console.log(resposta.query)
+
+    const salario = Number(requisicao.query.salario)
+    const reajuste = Number(requisicao.query.reajuste)
+    const total = ((salario * reajuste) / 100) + salario
+
+    resposta.send(`Seu salário era ${salario},<br>
+                depois de ${reajuste}%,
+                ficou ${total}`)
+
+})
+
+/*
+4. O custo de um carro novo ao consumidor é a soma do custo de fábrica com a porcentagem do distribuidor e dos impostos (aplicados ao custo de fábrica). Supondo que o percentual do distribuidor seja de 28% e os impostos de 45%,
+escrever um script para ler o custo de fábrica de um carro, calcular e escrever o custo final ao consumidor.
+*/
+
+app.get('/exercicio4', (requisicao, resposta) => {
+
+    console.log(resposta.query)
+
+    const custoFabrica = Number(requisicao.query.custoFabrica)
+    const imposto = 45
+    const distribuidor = 28
+
+    const custoFinal = (((imposto + distribuidor) * custoFabrica) / 100) + custoFabrica
+
+    resposta.send(`O custo de fábrica de seu carro é ${custoFabrica},<br>
+                 Com ${imposto}% + ${distribuidor}%,<br>
+                 ficará ${custoFinal}`)
+
+})
+
+
+
+/*5. O custo de um carro novo ao consumidor é a soma do custo de fábrica com
+a porcentagem do distribuidor e dos impostos (aplicados ao custo de fábrica). 
+Escreva um script para ler o custo de fábrica de um carro, a porcentagem do 
+distribuidor e o imposto, e calcular e escrever o custo final ao consumidor*/
+//Amanhã continuo em um arquivo diferente
+
+app.get('/exercicio5', (requisicao, resposta) => {
+    
+    console.log(resposta.query)
+    
+    const custoFab = Number(requisicao.query.custoFab)
+    const imp = Number(requisicao.query.imp)
+    const distri = Number(requisicao.query.distri)
+
+    const tudo = ((imp + distri) * custoFab / 100) + custoFab
+
+    resposta.send(`O custo de fábrica de seu veículo é de ${custoFab}, com ${imp}% de imposto + <br>
+                 ${distri}% de taxa de distribuição, <br>
+                 o valor final fica ${tudo}`)
+
+})
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Api iniciada! Rodando em http://localhost:3000') 
+
+
 })
