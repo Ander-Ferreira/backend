ae1994b2ed0577eefed41aa4f97e8114df4b79b8
Ander-Ferreira <andersonadm1314@gmail.com>
2024-03-28 22:12:28 -0300

    atualizando o arquivo contatos

--- 04-crude-de-contatos/routes/contatos.js
+++ 04-crude-de-contatos/routes/contatos.js
@@ -1,54 +1,46 @@
-//importar express
+// importar express
 const express = require('express')
-//criando um router
+// criar um router
 const router = express.Router()
 
-//dados mockados
-//criar uma lista
-let listaContatos = ["João", "Pedro", "Daniel"]
+// dados mockados
+// criar uma lista
+let listaContatos = []
 
-
-//CRUD
-//READ BUSCAR TODOS OS CONTATOS
-router.get('/contatos', (req, res)=>{
-
+// CRUD
+// READ - BUSCAR TODOS OS CONTATOS
+router.get('/contatos', (req, res) => {
     res.json(listaContatos)
 })
 
-
-
-//READ - BUSCA DO CONTATO PELO IDENTIFICADOR
-router.get('/contatos/:id', (req, res)=>{
+// READ - BUSCA DO CONTATO PELO IDENTIFICADOR
+router.get('/contatos/:id', (req, res) => {
     const id = req.params.id
     const contato = listaContatos[id]
     res.json(contato)
 })
 
-
-//CREATE = CADASTRAR UM CONTATO
-router.post('/contatos', (req, res) =>{
+// CREATE - CADASTRAR UM CONTATO
+router.post('/contatos', (req, res) => {
     const contato = req.body
     listaContatos.push(contato.nome)
-    res.status(201).json({ mensagem: "Contato Criado com Sucesso!" })
+    res.status(201).json({ mensagem: "Contato criado com sucesso!" })
 })
 
-
-//DELETE - EXCLUIR CONTATO
-router.delete('/contatos/:id', (req, res)=>{
+// DELETE - EXCLUIR CONTATO
+router.delete('/contatos/:id', (req, res) => {
     const id = req.params.id
     listaContatos.splice(id, 1)
-    res.json({mensagem: "contato excluído com sucesso!"})
+    res.json({ mensagem: "Contato excluido com sucesso!"})
 })
 
-//UPDATE - ATUALIZAR CONTATO
-router.put('/contatos/:id', (req, res)=>{
+// UPDATE - ATUALIZAR CONTATO 
+router.put('/contatos/:id', (req, res) => {
     const id = req.params.id
     const contato = req.body
     listaContatos[id] = contato.nome
-    res.json({mensagem: "Atualizado com sucesso"})
-
+    res.json({ mensagem: "Contato atualizado com sucesso!" })
 })
 
-//exportar o router
+// exportar o router
 module.exports = router
-
