1f3871504080160738c46a1bf5702e1d6f2e47b0
Anderson Ferreira <andersonadm1314@gmail.com>
2024-04-05 01:44:08 -0300

    renomeando

--- 03-lista-exercicios-02/index.js
+++ 03-lista-exercicios-02/index.js
@@ -213,6 +213,28 @@ Código do Cargo -> Percentual:
         res.json(saida)
     })
 
+    /*
+    9. Faça uma api que receba o valor do salário mínimo, o número de horas trabalhadas, o número de dependentes do funcionário e a quantidade de horas extras trabalhadas. Calcule e imprima o salário a receber do funcionário seguindo as regras abaixo:
+
+    •  Valor da hora trabalhada é igual a 1/5 do salário mínimo;
+    •  Salário do mês é igual ao número de horas trabalhadas vezes o valor da hora trabalhada;
+    •  Para cada dependente acréscimo de 32 reais;
+    •  Para cada hora extra trabalhada o cálculo do valor da hora trabalhada acrescida de 50 %;
+    •  Salário bruto é igual ao salário do mês mais os valores dos dependentes mais os valores das horas extras;
+    •  Cálculo do valor do imposto de renda retido na fonte segue a tabela abaixo:
+        IRRF | Salário Bruto
+        Isento Inferior a 2.000
+        10% De 2.000 a 5.000
+        20% Superior a 5.000
+    • Salário líquido é igual ao salário bruto menos IRRF;
+    • A gratificação segue a próxima tabela:
+          Salário Líquido | Gratificações
+          Até 3.500 | 1.000 reais
+          Superior a 3.500 | 500 reais
+    • Salário a receber do funcionário é igual ao salário líquido mais a gratificação.
+
+    */
+
 
 app.listen(3000, ()=>{
     console.log('aplicação iniciada em http://localhost:3000')
