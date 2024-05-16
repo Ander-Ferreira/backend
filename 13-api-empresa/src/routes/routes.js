b62da19557c5f65303cfcbf057497b510254bdf7
Ander-Ferreira <andersonadm1314@gmail.com>
2024-05-16 20:12:29 -0300

    commitando projeto 13

--- 13-api-empresa/src/routes/routes.js
+++ 13-api-empresa/src/routes/routes.js
@@ -5,11 +5,13 @@ const router = express.Router()
 
 const cargoController = require('../controllers/cargoControllers')
 const funcionarioController = require('../controllers/funcionarioControllers')
+const departamentoController = require('../controllers/departamentoControllers')
 
 //Importação de validators (validações)
 
 const {cargoVALIDADOR} = require('../validators/cargoValidator') //aperto dentro do objeto ctrl + espaço para selecionar nossa função, precisa ter sido exportado como objeto para aparecer.
 const {validarID} = require('../validators/idValidator')
+
 
 
 //Rotas de cargos
@@ -26,6 +28,16 @@ router.get('/funcionarios', validarID, funcionarioController.getAll)
 router.get('/funcionarios/:id', validarID, funcionarioController.getById)
 router.put('/funcionarios/:id', funcionarioController.update)
 router.delete('/funcionarios/:id', validarID, funcionarioController.remove)
+
+
+//Rotas Departamento
+
+router.post('/departamento', departamentoController.create)
+router.get('/departamento', departamentoController.getALL)
+router.get('/departamento/:id', departamentoController.getById)
+router.put('/departamento/:id', departamentoController.update)
+router.delete('/departamento/:id', departamentoController.remove)
+
 
 
 
