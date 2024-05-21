7408a1ed75f6c8aac25941c017fa153c6dfbd3ba
Anderson Ferreira <andersonadm1314@gmail.com>
2024-05-21 03:00:25 -0300

    Adição de autenticação e login na api com token

--- 13-api-empresa/src/index.js
+++ 13-api-empresa/src/index.js
@@ -5,15 +5,23 @@ const porta = 3000
 //Middlewares
 app.use(express.json())
 
-//Importações
-
 //Importação da conexão do banco de dados 
 const DBconnection = require('./db/connection')
 DBconnection()
 
+
+//Importações de rotas de autenticações
+const autenticacaoROUTER = require('./routes/autenticacao.routes')
+app.use(autenticacaoROUTER)
+
+
+//Checar token
+const {checarTOKEN} = require('./validators/autenticacaoValidator')
+
+
 //Importação de nossas rotas
 const router = require('./routes/routes')
-app.use(router)
+app.use('/', checarTOKEN, router)
 
 
 
@@ -21,4 +29,7 @@ app.use(router)
 app.listen(porta, ()=>{
     console.log('Aplicação rodando na porta', porta)
 })
+
+//Bibliotecas usadas neste projeto:
+//npm install express nodemon  dotenv mongoose jsonwebtoken bcrypt
 
